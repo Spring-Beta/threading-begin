0855088c6742f1661d79b85aa154265bd4ff359a
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-07-08 14:10:11 +0600

    multi threading

---
+++ src/test/java/com/example/threading/MultiThreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
